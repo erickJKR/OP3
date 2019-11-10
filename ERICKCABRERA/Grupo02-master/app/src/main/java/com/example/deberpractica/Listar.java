639794aaedafcbf1690e9675a0333f25c9cb84fb
dllara <dllara@uce.edu.ec>
2019-11-10 07:59:43 -0500

    Update Listar.java

--- ERICKCABRERA/Grupo02-master/app/src/main/java/com/example/deberpractica/Listar.java
+++ ERICKCABRERA/Grupo02-master/app/src/main/java/com/example/deberpractica/Listar.java
@@ -19,12 +19,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 public class Listar extends AppCompatActivity {
     private TextView usuariosListar;
     private Spinner sp1;
     private LeerArchivo archivos=new LeerArchivo();
-
+    ArrayList<String> usuariosDetalles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,21 +34,10 @@ public class Listar extends AppCompatActivity {
         usuariosListar = (TextView) findViewById(R.id.textView3);
         sp1=(Spinner)findViewById(R.id.spinner4);
 
-        final String usuariosDetalles[] =archivos.leer();
-        String usuariosAreglos[]= new String[usuariosDetalles.length];
-        String claveAreglos[] = new String[usuariosDetalles.length];
-        for (int i = 0; i < usuariosDetalles.length; i++) {
-            System.out.println(usuariosDetalles[i]+"leer");
-            String [] parts = usuariosDetalles[i].split(" ");
-            System.out.println(parts.length+"tamaño");
-            String usuarioi = parts[0];
-            System.out.println(usuarioi+" vector usuario");
-            String clavei = parts[1];
-            System.out.println(usuarioi + " - " + clavei);
-            usuariosAreglos[i] = usuarioi;
-            claveAreglos[i] = clavei;
-            // }
-        }
+        usuariosDetalles =archivos.leer();
+        ArrayList<String> usuariosAreglos= archivos.leerColumna(0);
+        ArrayList<String> claveAreglos= archivos.leerColumna(1);;
+
         //String [] usuaruiosAreglo ={"01","02","03","04","05","06","07","08","09","10","11","12"};
         ArrayAdapter<String> adapter1=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,usuariosAreglos);
         sp1.setAdapter(adapter1);
@@ -60,20 +50,25 @@ public class Listar extends AppCompatActivity {
                         Toast.makeText(spn.getContext(), "Has seleccionado " +
                                         spn.getItemAtPosition(posicion).toString(),
                                 Toast.LENGTH_LONG).show();
-                        String [] parts = usuariosDetalles[posicion].split(" ");
+                        String [] parts = usuariosDetalles.get(posicion).split(" ");
                         //System.out.println(parts.length+"tamaño");
-                        String usuarioi = parts[0];
-                        String clavei = parts[1];
-                        String nombre = parts[2];
-                        String apellido = parts[3];
-                        String email = parts[4];
-                        String telefono = parts[5];
-                        String genero = parts[6];
-                        String fecha=parts[7];
-                        String asignaturas = parts[8];
-                        String becado = parts[9];
-                        String detallesUsuarioElegido="nombre: "+nombre+"\n"+"apellido: "+apellido+"\n"+"email: "+email+"\n"+"telefono: "+telefono+"\n"+"genero: "+genero+"\n"+"fecha: "+fecha+"\n"+"asignatura: "+asignaturas+"\n"+"becado: "+becado;
-                        usuariosListar.setText(detallesUsuarioElegido);
+                        if (parts.length==10) {
+                            String usuarioi = parts[0];
+                            String clavei = parts[1];
+                            String nombre = parts[2];
+                            String apellido = parts[3];
+                            String email = parts[4];
+                            String telefono = parts[5];
+                            String genero = parts[6];
+                            String fecha = parts[7];
+                            String asignaturas = parts[8];
+                            String becado = parts[9];
+                            String detallesUsuarioElegido = "nombre: " + nombre + "\n" + "apellido: " + apellido + "\n" + "email: " + email + "\n" + "telefono: " + telefono + "\n" + "genero: " + genero + "\n" + "fecha: " + fecha + "\n" + "asignatura: " + asignaturas + "\n" + "becado: " + becado;
+                            usuariosListar.setText(detallesUsuarioElegido);
+                        }else{
+                            Toast.makeText(Listar.this,"ERROR DE ENTRADA DE DATOS",Toast.LENGTH_LONG).show();
+
+                        }
                     }
                     public void onNothingSelected(AdapterView<?> spn) {
                     }
