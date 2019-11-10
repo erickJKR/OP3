01b3b662ab9b5fe4a0342f7ba29f8be637496cb2
dllara <dllara@uce.edu.ec>
2019-11-10 00:00:43 -0500

    Update MainActivity.java

--- ERICKCABRERA/Grupo02-master/app/src/main/java/com/example/deberpractica/MainActivity.java
+++ ERICKCABRERA/Grupo02-master/app/src/main/java/com/example/deberpractica/MainActivity.java
@@ -25,12 +25,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText claveTextView;
     String usuarioTxt;
     String claveTxt;
-    private String archivo ="miarchivo7";
-    private String carpeta= "/archivos/";
-    File file;
-    String file_path="";
-    String name="";
-    ListaEstudiantes listarEstudiantes;
+    private LeerArchivo archivos=new LeerArchivo();
+        ListaEstudiantes listarEstudiantes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,24 +36,6 @@ public class MainActivity extends AppCompatActivity {
         btn2=(Button) findViewById(R.id.buttonRegistro);
         usuarioTextView = (EditText) findViewById(R.id.editText);//texto de ingreso de usuario
         claveTextView=(EditText)findViewById(R.id.editText2);//texto de ingreso de clave
-        this.file_path=(Environment.getExternalStorageDirectory()+this.carpeta);
-        File localFile=new File(this.file_path);
-        if(!localFile.exists()){
-            localFile.mkdir();
-        }
-        this.name=(this.archivo +".txt");
-        this.file=new File(localFile,this.name);
-        try{
-            this.file.createNewFile();
-            escribir("admin", "admin","david","lara","dllara@uce.edu.ec","0998124155","hombre","23/10/1995","Fisica-Ciencias-Informatica-","si");
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        //String usuariosDetalles [];
-        //String usuariosAreglos[];
-        //String claveAreglos[];
-
-
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,8 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 usuarioTxt= (String) usuarioTextView.getText().toString();
                 claveTxt= (String) claveTextView.getText().toString();
 
-                //if(file.exists()){
-                String usuariosDetalles[] =leer();
+                String usuariosDetalles[] =archivos.leer();
                 String usuariosAreglos[]= new String[usuariosDetalles.length];
                 String claveAreglos[] = new String[usuariosDetalles.length];
                 for (int i = 0; i < usuariosDetalles.length; i++) {
@@ -114,62 +92,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    public void escribir(String usua, String clave,String nombre,String apellido,String email,String telefono,String genero,String fecha,String asignaturas,String becado){
-        FileWriter fichero=null;
-        PrintWriter pw =null;
-        try{
-            //fichero=new FileWriter(file);
-            fichero=new FileWriter(file.getAbsoluteFile(), true);
-            pw=new PrintWriter(fichero);
-            //pw.println(usuarioTextView.getText().toString());
-            pw.println(usua+" "+clave+" "+nombre+" "+apellido+" "+email+" "+telefono+" "+genero+" "+fecha+" "+asignaturas+" "+becado);
-            pw.flush();
-            pw.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally{
-            try{
-                if (null!= fichero)
-                    fichero.close();
-            }catch (Exception e2){
-                e2.printStackTrace();
-            }
-        }
-    }
-    public  String[] leer(){
-        FileInputStream fin=null;
-        String matriz[] = new String[1000];
 
-        int n=0;
-        try{
-            fin= new FileInputStream(file);
-
-        }catch(FileNotFoundException e1){
-            e1.printStackTrace();
-        }
-        InputStreamReader archivo=new InputStreamReader(fin);
-        BufferedReader br=new BufferedReader(archivo);
-        try{
-            String linea;
-            int i = 0;
-            while ((linea = br.readLine()) != null) {
-                matriz[i] = linea;
-                i=i+1;
-            }
-            n=i;
-
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        String usuariosDetalles[] = new String[n];
-        for (int i = 0; i < n; i++) {
-            System.out.println(matriz[i]);
-            usuariosDetalles[i]  = matriz[i] ;
-            System.out.println(usuariosDetalles[i]+"metodo leer");
-        }
-        return usuariosDetalles;
-    }
 
 }
 
