c964dcecc2bb97c4998c8bd15f5ddfa107c3ba54
dllara <dllara@uce.edu.ec>
2019-11-10 00:00:31 -0500

    Update Listar.java

--- ERICKCABRERA/Grupo02-master/app/src/main/java/com/example/deberpractica/Listar.java
+++ ERICKCABRERA/Grupo02-master/app/src/main/java/com/example/deberpractica/Listar.java
@@ -23,11 +23,8 @@ import java.io.PrintWriter;
 public class Listar extends AppCompatActivity {
     private TextView usuariosListar;
     private Spinner sp1;
-    private String archivo ="miarchivo8";
-    private String carpeta= "/archivos/";
-    File file;
-    String file_path="";
-    String name="";
+    private LeerArchivo archivos=new LeerArchivo();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,21 +32,8 @@ public class Listar extends AppCompatActivity {
 
         usuariosListar = (TextView) findViewById(R.id.textView3);
         sp1=(Spinner)findViewById(R.id.spinner4);
-        this.file_path=(Environment.getExternalStorageDirectory()+this.carpeta);
-        File localFile=new File(this.file_path);
-        if(!localFile.exists()){
-            localFile.mkdir();
-        }
-        this.name=(this.archivo +".txt");
-        this.file=new File(localFile,this.name);
-        try{
-            this.file.createNewFile();
-            //escribir("admin", "admin","david","lara","dllara@uce.edu.ec","0998124155","hombre","23/10/1995","Fisica-Ciencias-Informatica-","si");
 
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        final String usuariosDetalles[] =leer();
+        final String usuariosDetalles[] =archivos.leer();
         String usuariosAreglos[]= new String[usuariosDetalles.length];
         String claveAreglos[] = new String[usuariosDetalles.length];
         for (int i = 0; i < usuariosDetalles.length; i++) {
@@ -97,73 +81,6 @@ public class Listar extends AppCompatActivity {
 
     }
 
-    public  String[] leer(){
-        String contenido="";
-        FileInputStream fin=null;
-        String matriz[] = new String[1000];
 
-        int n=0;
-        try{
-            fin= new FileInputStream(file);
-
-        }catch(FileNotFoundException e1){
-            e1.printStackTrace();
-        }
-        InputStreamReader archivo=new InputStreamReader(fin);
-        BufferedReader br=new BufferedReader(archivo);
-        int ascii;
-        try{
-            //while ((ascii=br.read())!= -1){
-            // char caracter=(char) ascii;
-            //contenido+=caracter;
-
-            //}
-            String linea;
-            int i = 0;
-            while ((linea = br.readLine()) != null) {
-                matriz[i] = linea;
-                i=i+1;
-            }
-            n=i;
-
-
-        }catch (IOException e){
-            e.printStackTrace();
-        }
-        //Log.e(tag="leer",contenido);
-
-        String usuariosDetalles[] = new String[n];
-        for (int i = 0; i < n; i++) {
-            System.out.println(matriz[i]);
-            usuariosDetalles[i]  = matriz[i] ;
-            System.out.println(usuariosDetalles[i]+"metodo leer");
-        }
-//       claveTextView.setText(usuariosDetalles[n-1]);
-        //System.out.println(usuariosDetalles[n-1]+"llllllllllllllllllllllllll");
-
-        return usuariosDetalles;
-    }
-    public void escribir(String usua, String clave,String nombre,String apellido,String email,String telefono,String genero,String fecha,String asignaturas,String becado){
-        FileWriter fichero=null;
-        PrintWriter pw =null;
-        try{
-            //fichero=new FileWriter(file);
-            fichero=new FileWriter(file.getAbsoluteFile(), true);
-            pw=new PrintWriter(fichero);
-            //pw.println(usuarioTextView.getText().toString());
-            pw.println(usua+" "+clave+" "+nombre+" "+apellido+" "+email+" "+telefono+" "+genero+" "+fecha+" "+asignaturas+" "+becado);
-            pw.flush();
-            pw.close();
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally{
-            try{
-                if (null!= fichero)
-                    fichero.close();
-            }catch (Exception e2){
-                e2.printStackTrace();
-            }
-        }
-    }
 }
 
