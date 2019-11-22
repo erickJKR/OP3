d1728dbf93171744c2575702f30cd34d79dc1de4
dllara <dllara@uce.edu.ec>
2019-11-21 20:56:49 -0500

    Update serv1.py

--- ERICKCABRERA/SERVICIOS/serv1.py
+++ ERICKCABRERA/SERVICIOS/serv1.py
@@ -9,19 +9,15 @@ def index():
 	else:
 		return jsonify({"about": "Hello Wordl!"})
 
-@app.route('/ope/<int:num>', methods=['GET'])
+@app.route('/oper/<int:num>', methods=['GET'])
 def mensaje1(num):
-        String mensaje;
-        switch (num) {
-            case 1:  mensaje = "Eliminado";
-                     break;
-            case 2:  mensaje = "Editado";
-                     break;
-            case 3:  mensaje = "Error";
-                     break;
-            default: mensaje = "Invalid mensaje";
-                     break;
-        }
+	mensaje="indefinido";
+	if(num==1):
+		mensaje="Editado";
+	if(num==2):
+		mensaje="Editado";
+	if(num==3):
+		mensaje="Error";
 	return jsonify({"about": mensaje})
 
 if __name__ == '__main__':
